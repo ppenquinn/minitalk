f6488f8ecea91dccdc44cefcbe531c1f2b936c38
ppenquinn <penguin32439@outlook.co.th>
2024-06-29 12:15:58 +0700

    finish

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: nappalav <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 17:54:51 by nappalav          #+#    #+#             */
-/*   Updated: 2024/06/27 18:39:20 by nappalav         ###   ########.fr       */
+/*   Updated: 2024/06/29 12:12:59 by nappalav         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,42 @@
 static int	handle_error(int code)
 {
 	if (code == 1)
-		ft_printf("Argument Error");
+		ft_printf("Argument Error\n");
 	else if (code == 2)
-		ft_printf("PID Error");
+		ft_printf("PID Error\n");
 	return (0);
+}
+
+static void ft_send_bit(int pid, char c)
+{
+	int bit;
+
+	bit = 7;
+	while (bit != -1)
+	{
+		if (c >> bit-- & 1)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		usleep(200);
+	}
 }
 
 int	main(int argc, char **argv)
 {
 	int	pid;
+	int	i;
 
 	if (argc != 3)
 		return (handle_error(1));
 	if (!ft_atoi_check(argv[1], &pid))
 		return (handle_error(2));
+	i = 0;
+	while (argv[2][i] != 0)
+	{
+		ft_send_bit(pid, argv[2][i]);
+		i++;
+	}
+	ft_send_bit(pid,'\n');
+	return (0);
 }
--- server.c
+++ server.c
@@ -6,14 +6,42 @@
 /*   By: nappalav <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 18:30:27 by nappalav          #+#    #+#             */
-/*   Updated: 2024/06/27 18:33:44 by nappalav         ###   ########.fr       */
+/*   Updated: 2024/06/29 12:15:44 by nappalav         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int main()
+static void	sig_handler(int signum, siginfo_t *info, void *context)
 {
-	ft_printf("sadfghj");
+	static int	c = 0;
+	static int	bit = 7;
+
+	(void)context;
+	(void)info;
+
+	c += (signum == SIGUSR1) << bit--;
+	if (bit == -1)
+	{
+		ft_printf("%c", c);
+		bit = 7;
+		c = 0;
+	}
+}
+
+int	main(void)
+{
+	struct sigaction	act;
+
+	ft_printf("Server PID: [%d]\n", getpid());
+	act.sa_sigaction = sig_handler;
+	act.sa_flags = SA_SIGINFO;
+	sigemptyset(&act.sa_mask);
+	while (1)
+	{
+		sigaction(SIGUSR1, &act, NULL);
+		sigaction(SIGUSR2, &act, NULL);
+		usleep(300);
+	}
 	return (0);
 }
